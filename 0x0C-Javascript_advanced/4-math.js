88a7f3e44e671c0e1b22bc880dfcb9104a3b3c3f
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2020-09-20 17:18:43 -0500

    Task 4

---
+++ 0x0C-Javascript_advanced/4-math.js
@@ -0,0 +1,14 @@
+const divideBy = (firstNumber) => {
+  return (secondNumber) => {
+    return secondNumber / firstNumber;
+  };
+};
+const addBy = (firstNumber) => {
+  return (secondNumber) => {
+    return firstNumber + secondNumber;
+  };
+};
+let addBy100 = addBy(100);
+let addBy1000 = addBy(1000);
+let divideBy10 = divideBy(10);
+let divideBy100 = divideBy(100);
