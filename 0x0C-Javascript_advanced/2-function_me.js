de7b90562763f0aa32a935d5f47fae99d949ed83
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2020-09-20 16:22:45 -0500

    Task 2

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,4 @@
+const welcomeMessage = (fullName) => alert(`Welcome ${fullName}`);
+let guillaume = welcomeMessage("Guillaume");
+let alex = welcomeMessage("Alex");
+let fred = welcomeMessage("Fred");
