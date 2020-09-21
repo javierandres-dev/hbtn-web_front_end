c8a378f87e6e57f3b31d7093a1e23a655ef7eafe
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2020-09-20 21:10:56 -0500

    Task 9

---
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -0,0 +1,19 @@
+const countPrimeNumbers = () => {
+  let amount = 0;
+  for (let divisor = 2; divisor <= 200; divisor++) {
+    let times = 0;
+    for (let dividend = 2; dividend <= divisor; dividend++) {
+      if (divisor % dividend == 0) times++;
+    }
+    if (times == 1) amount++;
+  }
+  return amount;
+};
+const start = performance.now();
+countPrimeNumbers();
+const end = performance.now();
+console.log(
+  `Execution time of printing countPrimeNumbers was ${
+    end - start
+  } milliseconds.`
+);
